3a854a47f102eca7014e1221b745bfeb95a7fa60
Hari <hnnhariharan12@gmail.com>
2023-04-28 06:30:59 +0530

    updated req.txt, train class initialized

--- train/train.py
+++ train/train.py
@@ -1,19 +1,20 @@
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.tune.logger import pretty_print
 
+class Train():
+    def train(self, epochs=10):
+        algo = (
+            PPOConfig()
+            .rollouts(num_rollout_workers=1)
+            .resources(num_gpus=0)
+            .environment(env="CartPole-v1")
+            .build()
+        )
 
-algo = (
-    PPOConfig()
-    .rollouts(num_rollout_workers=1)
-    .resources(num_gpus=0)
-    .environment(env="CartPole-v1")
-    .build()
-)
+        for i in range(epochs):
+            result = algo.train()
+            print(pretty_print(result))
 
-for i in range(10):
-    result = algo.train()
-    print(pretty_print(result))
-
-    if i % 5 == 0:
-        checkpoint_dir = algo.save()
-        print(f"Checkpoint saved in directory {checkpoint_dir}")
+            if i % 5 == 0:
+                checkpoint_dir = algo.save()
+                print(f"Checkpoint saved in directory {checkpoint_dir}")
